9280f7077b74aef0322d7da849c0b45a7fbf5c04
Krish Kittur <kkittur24@dtechhs.org>
2021-01-04 14:33:12 -0800

    Added some stuff

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -2,8 +2,11 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.XboxController.Button;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import edu.wpi.first.wpilibj2.command.button.POVButton;
 import frc.robot.commands.turret.ResetTurretCommand;
+import frc.robot.commands.turret.TurretToAngleCommand;
 import frc.robot.subsystems.TurretSubsystem;
 
 public class RobotContainer {
@@ -17,13 +20,42 @@ public class RobotContainer {
     configureButtonBindings();
 
     // Set the default commands
-  
+    turretSubsystem.setDefaultCommand(new RunCommand(() -> turretSubsystem.setTurretMotor(0), turretSubsystem));
   }
 
   private void configureButtonBindings() {
-    new JoystickButton(controller, Button.kA.value).whenHeld(
+    // If the 90 degree POV button is held, then set the Turret Motor at 3 volts
+    new POVButton(controller, 90).whenHeld(
+      new RunCommand(() -> turretSubsystem.setTurretMotor(3), turretSubsystem)
+    );
+
+    // If the 90 degree POV button is held, then set the Turret Motor at -3 volts
+    new POVButton(controller, 270).whenHeld(
+      new RunCommand(() -> turretSubsystem.setTurretMotor(-3), turretSubsystem)
+    );
+
+    // If the A button is pressed, then set the turret motor angle to 0 degrees
+    new JoystickButton(controller, Button.kA.value).whenPressed(
       new ResetTurretCommand(turretSubsystem)
     );
+
+    // If the B button is pressed, then set the turret motor angle to 90 degrees
+    new JoystickButton(controller, Button.kB.value).whenPressed(
+      new TurretToAngleCommand(turretSubsystem, 90)
+    );
+
+    // If the X button is pressed, then set the turret motor angle to -90 degrees
+    new JoystickButton(controller, Button.kX.value).whenPressed(
+      new TurretToAngleCommand(turretSubsystem, -90)
+    );
+
+    // If the Y button is pressed, then set the turret motor angle to 128 degrees
+    new JoystickButton(controller, Button.kY.value).whenPressed(
+      new TurretToAngleCommand(turretSubsystem, 138)
+    );
+
+
+    
     
   }
 
--- src/main/java/frc/robot/subsystems/TurretSubsystem.java
+++ src/main/java/frc/robot/subsystems/TurretSubsystem.java
@@ -4,13 +4,22 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel;
 
 import edu.wpi.first.wpilibj.DutyCycleEncoder;
+import edu.wpi.first.wpilibj.simulation.DutyCycleEncoderSim;
+import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.system.plant.DCMotor;
+import edu.wpi.first.wpilibj.util.Units;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class TurretSubsystem extends SubsystemBase {
 
     // Instantiate the hardware
     private final CANSparkMax turretMotor = new CANSparkMax(25, CANSparkMaxLowLevel.MotorType.kBrushless);
+    private final SingleJointedArmSim simTurretMotor = new SingleJointedArmSim(
+        DCMotor.getNEO(1), 25, 0.06, Units.inchesToMeters(6), Units.degreesToRadians(-10000), Units.degreesToRadians(10000), 0.425 * 3 + 1, false
+    );
     private final DutyCycleEncoder turretEncoder = new DutyCycleEncoder(4);
+    private final DutyCycleEncoderSim simTurretEncoder = new DutyCycleEncoderSim(turretEncoder);
 
     public TurretSubsystem() {
         // Set the setters
@@ -26,5 +35,14 @@ public class TurretSubsystem extends SubsystemBase {
     // Method to set the turret motors speed 
     public void setTurretMotor(double voltage) {
         turretMotor.setVoltage(voltage);
+        simTurretMotor.setInput(voltage);
+    }
+
+    // In the simulation periodic method, update the simulation motor, encoder, and add values to SmartDashboard
+    @Override
+    public void simulationPeriodic() {
+        simTurretMotor.update(0.020);
+        simTurretEncoder.setDistance(Units.radiansToDegrees(simTurretMotor.getAngleRads()));
+        SmartDashboard.putNumber("Motor Angle", readTurretEncoder());
     }
 }
