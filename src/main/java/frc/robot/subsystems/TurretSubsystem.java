e2328af41ef7c1fee06d903f1254e551f0c1929f
Krish Kittur <kkittur24@dtechhs.org>
2021-01-13 10:58:50 -0800

    pls work

--- src/main/java/frc/robot/subsystems/TurretSubsystem.java
+++ src/main/java/frc/robot/subsystems/TurretSubsystem.java
@@ -3,20 +3,12 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel;
 
-import edu.wpi.first.wpilibj.RobotBase;
-import edu.wpi.first.wpilibj.geometry.Pose2d;
-import edu.wpi.first.wpilibj.geometry.Rotation2d;
-import edu.wpi.first.wpilibj.geometry.Transform2d;
-import edu.wpi.first.wpilibj.geometry.Translation2d;
-import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import org.photonvision.PhotonCamera;
 import org.photonvision.PhotonPipelineResult;
 
 import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import org.photonvision.SimVisionSystem;
-import org.photonvision.SimVisionTarget;
 
 public class TurretSubsystem extends SubsystemBase {
 
@@ -25,59 +17,10 @@ public class TurretSubsystem extends SubsystemBase {
     private final DutyCycleEncoder turretEncoder = new DutyCycleEncoder(4);
     private final PhotonCamera camera = new PhotonCamera("Bigboy");
 
-    String camName = "Bigboy";
-    double cameDiagFOV = 75.0; // degrees
-    double camPitch = 0.0; // degrees
-    Transform2d cameraToRobot = new Transform2d(); // meters
-    double camHeightOffGround = 0.85; // meters
-    double maxLEDRange = 20; // meters
-    int camResolutionHeight = 640; // pixels
-    int camResolutionWidth = 480; // pixels
-    double minTargetArea = 10; // square pixels
-
-    SimVisionSystem simVision = RobotBase.isSimulation() ? new SimVisionSystem(camName,
-        cameDiagFOV,
-        camPitch,
-        cameraToRobot,
-        camHeightOffGround,
-        maxLEDRange,
-        camResolutionWidth,
-        camResolutionHeight,
-        minTargetArea) : null;
-    Field2d field = new Field2d();
-
     public TurretSubsystem() {
         // Set the setters
         turretMotor.setSmartCurrentLimit(25);
-        turretEncoder.setDistancePerRotation(360.0);
-
-        if(RobotBase.isSimulation()) {
-            var targetPose = new Pose2d(new Translation2d(15,3), new Rotation2d()); // meters
-            double targetHeightAboveGround = 2.3; // meters
-            double targetWidth = 0.54; // meters
-            double targetHeight = 0.25; // meters
-
-            var newTgt = new SimVisionTarget(targetPose,
-                targetHeightAboveGround,
-                targetWidth,
-                targetHeight);
-
-            simVision.addSimVisionTarget(newTgt);
-            field.setRobotPose(new Pose2d(2, 2, new Rotation2d()));
-            field.getObject("goal").setPose(targetPose);
-            SmartDashboard.putData("Field", field);
-        }
-    }
-
-    @Override
-    public void simulationPeriodic() {
-        simVision.processFrame(field.getRobotPose());
-
-        // Sketchy print we can remove
-        var tgt = camera.getLatestResult();
-        if(tgt.hasTargets()) {
-            System.out.println(tgt.getBestTarget().getYaw());
-        }
+        turretEncoder.setDistancePerRotation(360.0/1.0);
     }
 
     // Method to read the turret encoders values
